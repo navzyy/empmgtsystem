607a9b7f5146fa9c6068854015e138cc22238af0
Naveena Wimalarathne <naveenawimalarathne01@gmail.com>
2024-08-12 13:50:25 +0530

    initiam commit

---
+++ src/test/java/com/example/empmgmtsystem/EmpmgmtsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empmgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmgmtsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
